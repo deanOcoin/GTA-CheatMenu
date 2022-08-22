ac968f36a5fc40e23e7713bf74d04814e72c9e94
deano <77565607+deanOcoin@users.noreply.github.com>
2022-08-22 13:14:37 -0400

    Add files via upload

---
+++ cheatconfig.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "cheats.h"
+
+namespace Config
+{
+	inline std::string pname; // player name
+	inline float tGrav = 1.0; // vehicle gravity multiplier
+	
+	inline bool isMenuOn = 1;
+	inline bool isGodmode = 0;
+	inline bool isNeverWanted = 0;
+	inline bool isInfAmmo = 0;
+	inline bool isOSK = 0;
+	inline bool isZeroGrav;
+}
